3efcafc5e5f3b529cce7f52b8d23ffdc1a7e7014
Vikash Kumar <vikashplus@gmail.com>
2025-02-08 19:07:52 -0500

    FEATURE: Features offline as well as online previews

--- preview.py
+++ preview.py
@@ -3,65 +3,116 @@ import time
 
 from mujoco import viewer
 import os
+import click
+import skvideo.io
+from sys import platform
+import numpy as np
 
-def preview(XML_path):
-  XML_path = os.path.join(os.path.dirname(__file__), XML_path)
-  render = "onscreen"
-  # render = "offscreen"
-  width = 400
-  height = 400
-  mj_model = mujoco.MjModel.from_xml_path(XML_path)
-  mj_data = mujoco.MjData(mj_model)
+def get_aed(lookat: np.ndarray, camat: np.ndarray):
+    """
+    Calculate the camera azimuth, elevation, and distance from the given camera position.
 
+    Args:
+        lookat (np.ndarray): The point the camera is looking at, given as a 3D vector [x, y, z].
+        camat (np.ndarray): The camera position as a 3D vector [x, y, z].
 
-  if render == "onscreen":
-    window = viewer.launch_passive(
-        mj_model,
-        mj_data,
-        show_left_ui=False,
-        show_right_ui=False,
-    )
-  elif render=="offscreen":
-    renderer = mujoco.Renderer(mj_model, height=height, width=width)
-    scene_option = mujoco.MjvOption()
-    import ipdb; ipdb.set_trace()
+    Returns:
+        tuple: A tuple containing the azimuth (degrees), elevation (degrees), and distance.
+    """
+    # Calculate the vector from the lookat point to the camera position
+    delta = lookat - camat
 
+    # Calculate the distance
+    distance = np.linalg.norm(delta)
+    distance = 1e-3 if distance < 1e-3 else distance
 
-  tmax = 2
-  window.cam.elevation = -20
-  window.opt.geomgroup[3] = 1
+    # Calculate the azimuth angle in degrees
+    azimuth = np.degrees(np.arctan2(delta[1], delta[0]))
 
+    # Calculate the elevation angle in degrees
+    elevation = np.degrees(np.arcsin(delta[2] / distance))
 
-  # Update simulation
-  while window.is_running():
-
-    tt = mj_data.time
-
-    window.cam.azimuth = 90 + 360*tt/tmax # trick to rotate camera for 360 videos
-    # print(tt, window.cam.azimuth)
-
-    for gid in range(1, mj_model.ngeom):
-      geom = mj_model.geom(gid)
-      if geom.group == 3:
-        # geom.rgba[3] = min(tt/tmax, 1)
-        geom.rgba[3] = 1 if tt>tmax/2 else 0
-      else:
-        # geom.rgba[3] = max(1-tt/tmax, 0)
-        geom.rgba[3] = 1 if tt<tmax/2 else 0
+    return azimuth, elevation, distance
 
 
-    if tt>tmax:
-      mujoco.mj_resetData(mj_model, mj_data)
-      time.sleep(.1)
-      break
-
-    mujoco.mj_step(mj_model, mj_data)
-    window.sync()
-    time.sleep(mj_model.opt.timestep)
-  window.close()
+def preview(XML_path, render_mode="onscreen", duration=2):
+    XML_path = os.path.join(os.path.dirname(__file__), XML_path)
+    width = 384
+    height = 360
+    mj_model = mujoco.MjModel.from_xml_path(XML_path)
+    mj_data = mujoco.MjData(mj_model)
 
 
-import os
+    if render_mode == "onscreen":
+        window = viewer.launch_passive(
+            mj_model,
+            mj_data,
+            show_left_ui=False,
+            show_right_ui=False,
+        )
+        scene_option = window.opt
+        cam = window.cam
+
+    elif render_mode=="offscreen":
+        renderer = mujoco.Renderer(mj_model, height=height, width=width)
+        scene_option = mujoco.MjvOption()
+        cam = mujoco.MjvCamera()
+        rgb_frames = []
+
+    # Update scene
+    scene_option.geomgroup[3] = 1
+
+    # Update cam
+    cam.elevation = -20
+    cam.lookat=[0, 0, 0.04]
+
+    # Update simulation
+    tt = 0
+    nstep = int(duration//mj_model.opt.timestep)
+    nskip = int((1.0/30)//mj_model.opt.timestep)
+
+    for istep in range(nstep):
+
+        tt = mj_data.time
+
+        cam.azimuth = 90 + 360*tt/duration # trick to rotate camera for 360 videos
+
+        for gid in range(1, mj_model.ngeom):
+            geom = mj_model.geom(gid)
+            if geom.group == 3:
+                geom.rgba[3] = 1 if tt>duration/2 else 0
+            else:
+                geom.rgba[3] = 1 if tt<duration/2 else 0
+
+
+        # if tt>duration:
+        #     mujoco.mj_resetData(mj_model, mj_data)
+        #     time.sleep(.1)
+        #     break
+
+        mujoco.mj_step(mj_model, mj_data)
+        if render_mode == "onscreen":
+            window.sync()
+            time.sleep(mj_model.opt.timestep)
+
+        elif render_mode=="offscreen":
+            if istep % nskip == 0:
+                renderer.update_scene(mj_data, camera=cam, scene_option=scene_option)
+                rgb_frames.append(renderer.render())
+
+
+    if render_mode == "onscreen":
+        window.close()
+    else:
+        file_name =  f"videos/{XML_path.split('/')[-2]}.mp4"
+        print(f"saving: {file_name}")
+
+        # check if the platform is OS -- make it compatible with quicktime
+        if platform == "darwin":
+            skvideo.io.vwrite(file_name, np.asarray(rgb_frames),outputdict={"-pix_fmt": "yuv420p"})
+        else:
+            skvideo.io.vwrite(file_name, np.asarray(rgb_frames), outputdict={"-r": str(500)})
+
 
 def list_folders_in_path(path):
     """
@@ -86,19 +137,18 @@ def list_folders_in_path(path):
         return []
 
 
-
-import os
-import click
-
 @click.command(help="Preview Objects")
 @click.option('-o', '--object_name', type=str, default=None)
-def preview_objects(object_name):
+@click.option('-r', '--render_mode', type=click.Choice(['onscreen', 'offscreen']), default="onscreen")
+@click.option('-d', '--duration', type=float, default=2.0)
+def preview_objects(object_name, render_mode, duration):
     """
     Preview Objects
     """
 
+    tStart = time.time()
     if object_name:
-        preview(f"{object_name}/object.xml")
+        preview(f"{object_name}/object.xml", render_mode=render_mode, duration=duration)
     else:
         # find list of all the folder in the specified path
         path_to_search = os.path.dirname(__file__)
@@ -106,10 +156,11 @@ def preview_objects(object_name):
 
         for object_name in folders:
             if object_name != "google_objects":
-                print(object_name)
-                preview(f"{object_name}/object.xml")
-                time.sleep(.1)
-
+                preview(f"{object_name}/object.xml", render_mode=render_mode, duration=duration)
+                if render_mode=="onscreen":
+                    print(object_name)
+                    time.sleep(.1)
+    print(f"Total time spent:{time.time()-tStart}", flush=True)
 
 
 if __name__ == "__main__":
